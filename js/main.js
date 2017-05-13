0ce6b4dccb4e670cf41704a1c08ec23759b6bce5
Cuongtd <cuongtran.dc@gmail.com>
2017-05-13 13:57:39 +0700

    commit
    commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.list-menu ul').onePageNav();
+	
+	$(".button-fancy-single").fancybox({
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic',
+	});
+	
+	$(".button-fancy-video").fancybox({
+		/* maxWidth	: 800,
+		maxHeight	: 600, */
+		fitToView	: true,
+		/* width		: '70%',
+		height		: '70%', */
+		autoSize	: true,
+		closeClick	: false,
+		openEffect	: 'fade',
+		closeEffect	: 'fade'
+	});
+});
